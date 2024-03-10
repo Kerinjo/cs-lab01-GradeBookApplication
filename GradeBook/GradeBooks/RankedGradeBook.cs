982c897acba3cce265930350a6b5c5c766439678
Konrad Marciniak <konrad.marciniak@microsoft.wsei.edu.pl>
2024-03-10 23:08:10 +0100

    StandardGradeBook class finished, overridden GetLetterGrade method in RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            else
+            {
+                int greaterGradeCount = 0;
+                foreach (Student student in Students)
+                    if (student.AverageGrade > averageGrade)
+                        greaterGradeCount += 1;
+
+                int numTop20Percent = (int)(Students.Count * 0.2);
+                char gradeLetter = 'A';
+
+                for (int i = 1; i <= Students.Count; i++)
+                {
+                    if (i % numTop20Percent == 0)
+                        gradeLetter++;
+                }
+
+                return gradeLetter;
+            }
+        }
+    }
+}
